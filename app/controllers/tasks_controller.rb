e71b5b1de11562096657b8fc963f96f097c2c721
tatsuya.miyamoto <1to0saltangler1112@gmail.com>
2020-05-01 07:36:51 +0000

    Strong Parameter & flash

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,9 +47,12 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
+
+private
 
 # Strong Parameter
   def task_params
     params.require(:task).permit(:content)
   end
+
+end
